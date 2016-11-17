a9533b4151ba728025deebe666c31cd6dff8efbe
Jay Salmonson <salmonson1@llnl.gov>
2016-11-16 21:04:36 -0800

    Added shimmy; twisting movement, to dance step.

--- quadrille.py
+++ quadrille.py
@@ -27,28 +27,31 @@ class quadrille(object):
         speed = rospy.get_param("~speed", 0.5)
         turn  = rospy.get_param("~turn",  1.0)
 
-    def takeStep(self, dist, n_beats, forward):
-        t_dur  = n_beats*60./self.bpm
-        vel_ave = dist/t_dur # m/s
+    def takeStep(self, dist, n_beats, direction, theta_max):
+        t_dur      = n_beats*60./self.bpm
+        vel_ave    = dist/t_dur # m/s
+        shimmy_max = theta_max * pi / t_dur # max angular twist speed
         rate = rospy.Rate(10)  # Hz
         t = t0 = timer()
         # publish twist 0.0 here
         while (t < t0 + t_dur):
             rate.sleep()
             t = timer()
-            vel = forward * vel_ave * (1. - cos(2.*pi*(t-t0)/t_dur))
+            vel = direction * vel_ave * (1. - cos(2.*pi*(t-t0)/t_dur))
+            shimmy = shimmy_max * sin(2.*pi*(t-t0)/t_dur)
             twist = Twist()
             twist.linear.x = vel
             self.pub.publish(twist)
             print (t-t0), vel, t_dur
         twist = Twist()
         twist.linear.x = 0.
+        twist.angular.z = shimmy
         self.pub.publish(twist)
 
     def stepForward(self, dist = 0.5, n_beats = 2):
-        self.takeStep(dist, n_beats, 1)
+        self.takeStep(dist, n_beats, 1, 0)
     def stepBackward(self, dist = 0.5, n_beats = 2):
-        self.takeStep(dist, n_beats, -1)
+        self.takeStep(dist, n_beats, -1, 0)
 
 if __name__=="__main__":
 
@@ -56,5 +59,7 @@ if __name__=="__main__":
     try:
         dance.stepForward()
         dance.stepBackward()
+        dance.takeStep(0.5, 2, 1, pi/4.)
+        dance.takeStep(0.5, 2, -1, pi/4.)
     except rospy.ROSInterruptException:
         print "Exception"
