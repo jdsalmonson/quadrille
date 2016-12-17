6320de592d7593052629ba3b2085c069ad9caa9b
Jay Salmonson <salmonson1@llnl.gov>
2016-12-17 00:09:45 -0800

    Some bug fixes to LED service.

--- quadrille.py
+++ quadrille.py
@@ -16,6 +16,8 @@ from timeit import default_timer as timer
 from math import cos, pi
 import sys
 
+sys.path.append("../ros_arduino_bridge")
+from ros_arduino_msgs.srv import *
 
 class quadrille(object):
 
@@ -35,9 +37,9 @@ class quadrille(object):
 
         self.led_srv = rospy.ServiceProxy('digital_write',DigitalWrite)
 
-    def led_time(time):
+    def led_time(self, time):
         t_song    = time - self.t_start
-        n_beat    = int(t_song)/self.t_spb) + 1 #(1st is 1)
+        n_beat    = int(t_song/self.t_spb) + 1 #(1st is 1)
         n_measure = int(n_beat/self.meter) + 1  # measure number (1st is 1)
         # count of current measure:
         n_count   = n_beat - self.meter * (n_measure - 1)
@@ -48,7 +50,7 @@ class quadrille(object):
                 if self.led1_on is False:
                     self.led1_on = True
                     self.led_srv(pin=103,value=1)
-                if self.led2_on is True
+                if self.led2_on is True:
                     self.led2_on = False
                     self.led_srv(pin=104,value=0)
             else:
@@ -68,7 +70,7 @@ class quadrille(object):
 
 
     def takeStep(self, dist, n_beats, direction, theta_max):
-        t_dur      = n_beats*self.spb
+        t_dur      = n_beats*self.t_spb
         vel_ave    = dist/t_dur # m/s
         shimmy_max = theta_max * pi / t_dur # max angular twist speed
         rate = rospy.Rate(10)  # Hz
