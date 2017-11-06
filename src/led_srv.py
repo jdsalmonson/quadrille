2ba1c5e49fc7d3c673e605c73f8d08c7036edadf
Jay Salmonson <salmonson1@llnl.gov>
2017-11-05 22:43:48 -0800

    Completed implementation of LED_Srv class to blink LEDs

--- src/led_srv.py
+++ src/led_srv.py
@@ -11,78 +11,53 @@ from math import pi
 
 import rospy
 
-sys.path.append("../../ros_arduino_bridge")
-from ros_arduino_msgs.srv import DigitalWrite  # * 
-
-#from std_msgs.msg import Int16
-##sys.path.append("../../stdr_simulator")
-#from stdr_msgs.srv import ReadSensorPose, WriteSensorPose
-
-ms = 1.e-3 # [milliseconds/second]
+try:
+    sys.path.append("../../ros_arduino_bridge")
+    from ros_arduino_msgs.srv import DigitalWrite
+except:
+    pass
 
 class LED_Srv(object):
+    '''A class governing a ROS timer that blinks two LEDs at a given tempo
+    (given in seconds per beat): LED #1 on the "one" beat and LED #2
+    on all subsequent beats.
 
-    def __init__(self, t_spb, i_on_frac, meter, N_leds = 2):
+    t_spb (float) seconds-per-beat -- time between song beats
+    i_on_frac (int) inverse fraction of a single beat for which the LED is on
+    meter (int) numerator of key time signature. e.g. 3 in 3/4.
+    debug (False) opt. turn-on debug print statements.
+    '''
+    
+    def __init__(self, t_spb, i_on_frac, meter, debug = False):
         self.t_spb = t_spb #: [s] seconds per beat
         self.i_on_frac = i_on_frac #: inverse fraction of beat for which LED is on
         self.meter = meter
         self.led_dur = t_spb/i_on_frac #: [s] time to keep LED on
-        #self.pubrate = 10 #: [Hz] rate of servo position publication
 
         self.t_start = None
         
         self.led1 = 103
         self.led2 = 104
 
-        #self.N_leds = N_leds #: number of leds
-
+        self.debug = debug
+        
         def dummy(pin, value):
             pass
 
         try:
-            self.led_srv = rospy.ServiceProxy('/arduino/digital_write',foo) #DigitalWrite)
+            self.led_srv = rospy.ServiceProxy('/arduino/digital_write',DigitalWrite)
+            self.led_srv(pin=self.led1, value=0) # test LED to except out
         except:
             print("Warning: No LEDs available")
             self.led_srv = dummy
 
-        '''
-        self.leds = []
-        basenm = '/robot0/sonar_'
-        for isrv in range(N_servos):
-            servo = {}
-
-            servo['index'] = isrv
-            
-            servo['read_srv'] = \
-                rospy.ServiceProxy(basenm+str(isrv)+'/readpose',
-                                   ReadSensorPose)
-            servo['write_srv'] = \
-                rospy.ServiceProxy(basenm+str(isrv)+'/writepose',
-                                   WriteSensorPose)
-            
-            rd_srv = servo['read_srv']()
-            servo['pose'] = rd_srv.pose
-            servo['th_srv'] = rd_srv.pose.theta # servo->robot angle [rad]
-
-            # The following are in the servo reference frame [deg]:
-            servo['theta_ref'] = 90 # center, reference angle of servo
-            servo['theta_req'] = servo['theta_ref'] # requested theta
-
-            servo['theta'] = servo['theta_ref'] # actual servo theta
-
-            servo['pub_theta'] = rospy.Publisher(basenm+str(isrv)+'/angle',
-                                                 Int16, queue_size = 2)
-            
-            self.servos.append(servo)
-        '''
-        
     def startLEDS(self):
-        #self.t_start = rospy.get_time()
         # Init timer for leds:
-        print("led_dur = ",self.led_dur," ms = ",ms)
         self.timer = rospy.Timer(rospy.Duration(self.led_dur), self.onTimer)
-        # Init timer for publication of servo position:
-        #self.timerpub = rospy.Timer(rospy.Duration(1./self.pubrate), self.onTimerpub)
+        
+    def leds_off(self):
+        self.led_srv(pin=self.led1, value=0)
+        self.led_srv(pin=self.led2, value=0)
         
     def onTimer(self, event):
         time = event.current_real.to_sec()
@@ -96,85 +71,33 @@ class LED_Srv(object):
         # count of current measure:
         n_count   = n_beat - self.meter * (n_measure - 1)
 
-        # NEED to think about quantization:
-        # n_beat = int(round(t_song/self.t_spb)) + 1 ??
-        # something like this:
         n_beat_seg = int(round(t_song*self.i_on_frac/self.t_spb))
-        print("n_beat_seg = ",n_beat_seg, "n_count = ",n_count," t_song = ",t_song," residual = ",t_song/self.t_spb % self.t_spb)
-        #print("t_song = ",t_song)
+        if self.debug: print("n_beat_seg = ",n_beat_seg, "n_count = ",n_count," t_song = ",t_song," residual = ",t_song/self.t_spb % self.t_spb)
         if n_beat_seg % self.i_on_frac == 0:
             if n_count == 1:
                 self.led_srv(pin=self.led1, value=1) # turn on LED 1
-                print("LED 1: ON")
+                if self.debug: print("LED 1: ON")
             else:
                 self.led_srv(pin=self.led2, value=1) # turn on LED 2
-                print("LED 2: ON")
+                if self.debug: print("LED 2: ON")
         elif n_beat_seg % self.i_on_frac == 1:
             #: Could simplify this; just turn off all LEDs:
             if n_count == 1:
                 self.led_srv(pin=self.led1, value=0)
-                print("LED 1: OFF")
+                if self.debug: print("LED 1: OFF")
             else:
                 self.led_srv(pin=self.led2, value=0)
-                print("LED 2: OFF")
-        
-        '''
-        # could use 
-        cr = event.current_real.to_sec()
-        try:
-            lr = event.last_real.to_sec()
-        except:
-            lr = cr
-        dt = lr - cr
-        delt = dt
-        '''
-        #print("{} {} {} {}".format(event.current_real, event.last_real, event.last_duration, cr - lr))  #, event.current_real - event.last_real))
+                if self.debug: print("LED 2: OFF")
 
-        """
-        # move servo thetas:
-        myend = ''
-        for srv in self.servos:
-            if not srv['theta'] == srv['theta_req']:            
-                if srv['theta'] - srv['theta_req'] > 0.9*self.delt:
-                    srv['theta'] -= self.delt
-                elif srv['theta'] - srv['theta_req'] < -0.9*self.delt:
-                    srv['theta'] += self.delt
-                else:
-                    srv['theta'] = srv['theta_req']
-
-                dth = (srv['theta'] - srv['theta_ref'])*pi/180.
-                srv['pose'].theta = srv['th_srv'] + dth
-                srv['write_srv'](srv['pose'])
-                
-            #print(" {} {}  ".format(srv['theta_req'], srv['theta']), end=myend)
-            myend = "\n"
-            
-    def onTimerpub(self, event):
-        for srv in self.servos:
-            msg = Int16()
-            msg.data = srv['theta']
-            srv['pub_theta'].publish(msg)
-
-    def __call__(self, id, value):
-        self.servos[id]['theta_req'] = min(max(int(value), 0), 180)
-        """
 
 if __name__ == "__main__":
 
     import time
     
     rospy.init_node("led_node")
-    led_srv= LED_Srv(0.25, 4, 3)
+    led_srv= LED_Srv(0.25, 4, 3, debug = True)
 
     led_srv.startLEDS()
-
+    
+    # LED status will be updated over the duration of the sleep...
     time.sleep(1)
-    '''
-    sss = Servo_Srv_Sim()
-    sss(0,2)
-    sss(1,178)
-    time.sleep(1.5)
-    sss(0,43)
-    sss(1,87)
-    time.sleep(1)
-    '''
--- src/quadrille.py
+++ src/quadrille.py
@@ -18,82 +18,30 @@ from timeit import default_timer as timer
 from math import sin, cos, pi
 import sys
 
-# THIS ALL IS in led_srv:
-#sys.path.append("../../ros_arduino_bridge")
-#from ros_arduino_msgs.srv import *
-
 from led_srv import LED_Srv
 
-class quadrille(object):
+class Quadrille(object):
+    """A ROS node to perform a dance via execution of a sequence of Twist
+    commands.  If available, LEDs will be blinked to the rhythm.
+    
+    bpm (float) beats per minute -- tempo of song.
+    meter (int) numerator of key time signature. e.g. 3 in 3/4.
 
+    """
+    
     def __init__(self, bpm = 60, meter = 4):
         self.bpm   = bpm
         self.meter = meter  # numerator of key time signature. e.g. 3 in 3/4.
         self.t_start = -1   # time of start of entire dance
         self.t_spb   = 60./self.bpm  # seconds per beat
 
-        self.led_srv = LED_Srv(t_spb = t_spb, i_on_frac = 4, meter = meter)
-
-        '''  ALL OF THIS goes into LED_Srv():
-        self.led_dur = 0.25*self.t_spb  # time to keep LED on
-        self.led1 = 103
-        self.led2 = 104
-        self.led1_on = False
-        self.led2_on = False
-        '''
+        self.led_srv = LED_Srv(t_spb = self.t_spb, i_on_frac = 4, meter = meter)
         
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
         rospy.init_node('quadrille')
 
         #speed = rospy.get_param("~speed", 0.5)
         #turn  = rospy.get_param("~turn",  1.0)
-        ''' THIS ALL goes into LED_Srv():
-        def dummy(pin, value):
-            pass
-
-        try:
-            self.led_srv = rospy.ServiceProxy('/arduino/digital_write',DigitalWrite)
-        except:
-            print("Warning: No LEDs available")
-            self.led_srv = dummy
-        '''
-
-    # ALL THIS GOES INTO led_srv:
-    def led_time(self, time):
-        t_song    = time - self.t_start
-        n_beat    = int(t_song/self.t_spb) + 1 #(1st is 1)
-        n_measure = int(n_beat/self.meter) + 1  # measure number (1st is 1)
-        # count of current measure:
-        n_count   = n_beat - self.meter * (n_measure - 1)
-
-        # led = on if time since last beat is < led duration
-        if (t_song - (n_beat - 1)*self.t_spb) < self.led_dur:
-            if n_count == 1:
-                if self.led1_on is False:
-                    self.led1_on = True
-                    self.led_srv(pin=self.led1,value=1)
-                if self.led2_on is True:
-                    self.led2_on = False
-                    self.led_srv(pin=self.led2,value=0)
-            else:
-                if self.led1_on is True:
-                    self.led1_on = False
-                    self.led_srv(pin=self.led1,value=0)
-                if self.led2_on is False:
-                    self.led2_on = True
-                    self.led_srv(pin=self.led2,value=1)
-        else: # turn them both off:
-            if self.led1_on is True:
-                self.led1_on = False
-                self.led_srv(pin=self.led1,value=0)
-            if self.led2_on is True:
-                self.led2_on = False
-                self.led_srv(pin=self.led2,value=0)
-
-    def led_off(self):
-        self.led_srv(pin=self.led1,value=0)
-        self.led_srv(pin=self.led2,value=0)
-
 
     def takeStep(self, dist, n_beats, direction, theta_max):
         t_dur      = n_beats*self.t_spb
@@ -101,13 +49,14 @@ class quadrille(object):
         shimmy_max = theta_max * pi / t_dur # max angular twist speed
         rate = rospy.Rate(20)  # Hz
         t = t0 = timer()
-        # start master timer:
-        if self.t_start == -1: self.t_start = t
+        # start master timer & LEDs:
+        if self.t_start == -1:
+            self.t_start = t
+            self.led_srv.startLEDS()
         # publish twist 0.0 here
         while (t < t0 + t_dur):
             rate.sleep()
             t = timer()
-            self.led_time(t) # blink meter LEDs
             vel = direction * vel_ave * (1. - cos(2.*pi*(t-t0)/t_dur))
             shimmy = shimmy_max * sin(2.*pi*(t-t0)/t_dur)
             twist = Twist()
@@ -125,10 +74,17 @@ class quadrille(object):
     def stepBackward(self, dist = 0.5, n_beats = 2):
         self.takeStep(dist, n_beats, -1, 0)
 
+    def done(self):
+        self.led_srv.leds_off()
+        twist = Twist()
+        twist.linear.x = 0.
+        twist.angular.z = 0.
+        self.pub.publish(twist)        
+        
 if __name__=="__main__":
 
     # Nutcracker: Waltz of Flowers
-    dance = quadrille(bpm = 190, meter = 3)
+    dance = Quadrille(bpm = 190, meter = 3)
     try:
         print("A")
         dance.stepForward(0.25, 6)
@@ -143,6 +99,7 @@ if __name__=="__main__":
         dance.takeStep(0.125, 6, 1, 0.2 * pi/4.)
         dance.takeStep(0.125, 6, 1, -0.2 * pi/4.)
 
-        dance.led_off()
+        dance.done()
+        
     except rospy.ROSInterruptException:
         print("Exception")
